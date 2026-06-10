e4d896d517eb2a3b5b6bedefdd26b70b88e7960c
Divesh Kuthe <151843097+divesh012@users.noreply.github.com>
2026-06-10 16:51:44 +0530

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Loader initialized...");
+  
+    // Simulate loading timeout
+    setTimeout(() => {
+      // Hide the loader
+      document.querySelector('.background').style.display = 'none';
+  
+      // Display the canvas animation and main content
+      document.getElementById('animationCanvas').style.display = 'block';
+      document.querySelector('.main-content').style.display = 'block';
+  
+      // Initialize the canvas animation
+      initializeCanvasAnimation();
+    }, 3000); // Adjust timeout duration as needed
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    const heading = "Project's";
+    let index = 0;
+    const speed = 450; // Speed in milliseconds
+
+    function typeWriter() {
+        if (index < heading.length) {
+            document.getElementById("animated-heading").innerHTML += heading.charAt(index);
+            index++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    typeWriter();
+});
+  
